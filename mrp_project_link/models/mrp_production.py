a079823f5d250388586b87a7ac7f6448d91273d9
Arnaud LAYEC <arnaudlayec@gmail.com>
2024-12-29 21:42:48 +0100

    mrp_project display name

--- mrp_project_link/models/mrp_production.py
+++ mrp_project_link/models/mrp_production.py
@@ -5,8 +5,12 @@ from odoo import models, fields
 class WorkOrder(models.Model):
     _name = "mrp.production"
     _inherit = ['mrp.production', 'project.default.mixin']
+    _rec_name = 'display_name'
 
     description = fields.Char(
         string='Description'
     )
     
+    def _compute_display_name(self):
+        for mo in self:
+            mo.display_name = '[{}] {}' . format(self.name, self.description) 
