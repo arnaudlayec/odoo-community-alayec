53f7745b5c726ba8485c172f48e93b92ea8f8416
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-02-08 07:50:47 +0100

    fix account.move validation issue of bills not created from SO or PO

---
+++ account_move_tier_validation_reviewer/models/account_move.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = ['account.move']
+
+    @api.depends('move_type')
+    def _compute_invoice_default_sale_person(self):
+        """ Since `invoice_user_id` is used for validation process, it must not be
+            empty else it will trigger a User Error from `account_move_tier_validation`.
+            This happens for `account.move` created other than from SO or PO (e.g. HR expense)
+        """
+        super()._compute_invoice_default_sale_person()
+
+        for move in self:
+            if not move.invoice_user_id:
+                move.invoice_user_id = self.env.user
--- account_move_tier_validation_reviewer/readme/DESCRIPTION.rst
+++ account_move_tier_validation_reviewer/readme/DESCRIPTION.rst
@@ -3,3 +3,6 @@ This module adds *Reviewers* field on Account Move and
 pre-populate it from Purchase and Sale Orders seller or buyer.
 
 This is convenient when only original buyer or seller must be validator of the invoice.
+Note: since this module re-uses `invoice_user_id` field of `account.move`, it also
+sets a default value to the current user as validator for any bills not created from
+SO or PO. 
