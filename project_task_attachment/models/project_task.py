8a1c6f9a9201b933d2a4b4c6a3401c78555c2f2c
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-02-25 16:02:54 +0100

    fix task attachment_ids

--- project_task_attachment/models/project_task.py
+++ project_task_attachment/models/project_task.py
@@ -5,10 +5,10 @@ from odoo import models, fields
 class ProjectTask(models.Model):
     _inherit = ["project.task"]
 
-    attachment_ids = fields.One2many(
+    all_attachment_ids = fields.One2many(
+        # (!) `attachment_ids` is native (non-message attachments)
         comodel_name='ir.attachment',
         inverse_name='res_id',
         string='Attachments',
         domain=lambda self: [('res_model', '=', self._name)],
-        readonly=False
     )
