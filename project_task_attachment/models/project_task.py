0012f1f4d6fd27e470259127ca5b203ade4aadaf
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-02-25 15:54:00 +0100

    [debug] task attachment_ids : force_save

--- project_task_attachment/models/project_task.py
+++ project_task_attachment/models/project_task.py
@@ -3,12 +3,12 @@
 from odoo import models, fields
 
 class ProjectTask(models.Model):
-    _name = 'project.task'
     _inherit = ["project.task"]
 
     attachment_ids = fields.One2many(
         comodel_name='ir.attachment',
         inverse_name='res_id',
         string='Attachments',
-        domain=[('res_model', '=', _name)],
+        domain=lambda self: [('res_model', '=', self._name)],
+        readonly=False
     )
