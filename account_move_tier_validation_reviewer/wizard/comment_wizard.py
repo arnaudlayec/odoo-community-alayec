9ca2a410103df0c6b3ccb838752e9ed1fb438334
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-02-28 15:58:46 +0100

    notification on validation

--- account_move_tier_validation_reviewer/wizard/comment_wizard.py
+++ account_move_tier_validation_reviewer/wizard/comment_wizard.py
@@ -8,17 +8,19 @@ class CommentWizard(models.TransientModel):
     def add_comment(self):
         """ Send notification on chatter on validation """
         res = super().add_comment()
-        mail_values = self._get_chatter_validation_message()
-        self.message_post(**mail_values)
+
+        record = self.env[self.res_model].browse(self.res_id)
+        mail_values = self._get_chatter_validation_message(record)
+        record.message_post(**mail_values)
 
         return res
     
-    def _get_chatter_validation_message(self):
-        status = dict(self._fields['validation_status'].selection).get(self.validation_status)
+    def _get_chatter_validation_message(self, record):
+        status = dict(record._fields['validation_status'].selection).get(record.validation_status)
         return {
             'message_type': 'notification',
             'subtype_xmlid': 'mail.mt_note',
             'is_internal': True,
             'partner_ids': [],
-            'body': _("Validation status: %s (%s)", status, self.comment),
+            'body': _("Validation status: %s (%s)", status, record.comment),
         }
