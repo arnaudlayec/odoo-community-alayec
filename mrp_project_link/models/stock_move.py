4767adbbf7a43649ed98ba7e82cec59843db51ac
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-03-19 13:15:39 +0100

    fix empty `project_id` on svl

--- mrp_project_link/models/stock_move.py
+++ mrp_project_link/models/stock_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, exceptions, _
+from odoo import models, fields, api, exceptions, _
 
 class StockMove(models.Model):
     _inherit = ['stock.move']
