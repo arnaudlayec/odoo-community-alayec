aa254c69e9c3f939ba46f74dca3e2934c59940a8
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-05-16 09:58:44 +0200

    [FIX] precision rounding € instead of uom

--- stock_valuation_no_zero/models/product_product.py
+++ stock_valuation_no_zero/models/product_product.py
@@ -13,10 +13,11 @@ class ProductProduct(models.Model):
         if self._context.get('create_product_product'):
             return
         
+        prec = self.env['decimal.precision'].precision_get('Product Price')
         zero_standard_price = self.filtered(lambda product:
             not product.standard_price or float_is_zero(
                 product.standard_price,
-                precision_rounding=product.uom_id.rounding
+                precision_rounding = prec
             )
         )
         if zero_standard_price:
