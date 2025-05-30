432b1418d835f466f089ca86dd372900b7e95e79
Arnaud LAYEC <arnaudlayec@gmail.com>
2025-05-30 11:25:20 +0200

    [REVERSE] canNOT write cost_history only with analytic_account_id

--- project_budget_workforce/models/cost_history.py
+++ project_budget_workforce/models/cost_history.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
+from odoo.osv import expression
 from odoo.tools import date_utils
 
 class HrEmployeeTimesheetCostHistory(models.Model):
@@ -12,7 +13,6 @@ class HrEmployeeTimesheetCostHistory(models.Model):
         comodel_name="account.analytic.account",
         compute='_compute_analytic_account_id',
         store=True,
-        readonly=False,
     )
     date_to = fields.Date(
         string='Date To',
@@ -22,10 +22,17 @@ class HrEmployeeTimesheetCostHistory(models.Model):
     #===== Compute =====#
     # TO IMPROVE : we could use a related field to store employee_id, department_id, workcenter_id, etc...
     # to be agnostic of the model having a cost history and free the module manifest dependencies
+    def _get_fields_related(self):
+        return {
+            'employee_id': 'hr.employee',
+            'department_id': 'hr.department',
+            'workcenter_id': 'mrp.workcenter',
+        }
+    
     @api.depends(
-        'employee_id', 'employee_id.analytic_account_id',
-        'department_id', 'department_id.analytic_account_id',
-        'workcenter_id', 'workcenter_id.costs_hour_account_id',
+        'employee_id.analytic_account_id',
+        'department_id.analytic_account_id',
+        'workcenter_id.costs_hour_account_id',
     )
     def _compute_analytic_account_id(self):
         for history in self:
@@ -36,8 +43,8 @@ class HrEmployeeTimesheetCostHistory(models.Model):
             )
 
     @api.depends(
-        'department_id.timesheet_cost_history_ids.starting_date',
         'employee_id.timesheet_cost_history_ids.starting_date',
+        'department_id.timesheet_cost_history_ids.starting_date',
         'workcenter_id.cost_history_ids.starting_date',
     )
     def _compute_date_to(self):
@@ -50,36 +57,42 @@ class HrEmployeeTimesheetCostHistory(models.Model):
         no_starting_date.date_to = fields.Date.today()
         self = self - no_starting_date
         if not self.ids: # early quit for perf., if already finished
+            self.date_to = False
             return
         
         # Get `starting_date`, per employee, department or workcenter
+        fields_related = self._get_fields_related()
+        domain = expression.OR([
+            [(field, 'in', self[field].ids)] for field in fields_related
+        ])
         rg_result = self.sudo().read_group(
-            domain=['|',
-                ('employee_id', 'in', self.employee_id.ids),
-                ('department_id', 'in', self.department_id.ids),
-                ('workcenter_id', 'in', self.workcenter_id.ids),
-            ],
-            groupby=['employee_id', 'department_id', 'workcenter_id'],
+            domain=domain,
+            groupby=fields_related,
             fields=['starting_dates:array_agg(starting_date)'],
             lazy=False
         )
-        mapped_data = {'employee_id': {}, 'department_id': {}, 'workcenter_id': {}}
+        mapped_data = {field: {} for field in fields_related}
         for x in rg_result:
             field = self._get_field(x)
             mapped_data[field][x[field][0]] = x['starting_dates']
 
         for history in self:
             field = history._get_field()
-            dates_from = mapped_data[field].get(history[field].id, []) # all `starting_date` of siblings history
-            next_starting_date = [date for date in dates_from if date and date > history.starting_date]
-            history.date_to = date_utils.subtract(min(next_starting_date), days=1) if next_starting_date else False
+            if field:
+                dates_from = mapped_data[field].get(history[field].id, []) # all `starting_date` of siblings history
+                if dates_from:
+                    next_starting_date = [date for date in dates_from if date and date > history.starting_date]
+                    if next_starting_date:
+                        history.date_to = date_utils.subtract(min(next_starting_date), days=1)
+                        continue
+            history.date_to = False
     
     def _get_field(self, vals=None):
         is_dict = bool(vals)
         if not is_dict:
             vals = self
         
-        for field in ['employee_id', 'department_id', 'workcenter_id']:
+        for field in self._get_fields_related():
             if vals[field] and (is_dict and vals[field][0] or vals[field].id):
                 return field
     
@@ -88,11 +101,7 @@ class HrEmployeeTimesheetCostHistory(models.Model):
         """ On history costs tree view from Accounting app, to open Department or Employee form """
 
         field = self._get_field()
-        model = {
-            'workcenter_id': 'mrp.workcenter',
-            'department_id': 'hr.department',
-            'employee_id': 'hr.employee',
-        }.get(field)
+        model = self._get_fields_related().get(field)
         return {
             'type': 'ir.actions.act_window',
             'res_model': model,
