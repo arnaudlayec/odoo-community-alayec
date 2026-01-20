a68163d0b1c7f65e0ee83386cde3cbea4ae0bb22
Arnaud LAYEC <arnaudlayec@gmail.com>
2026-01-20 13:08:06 +0100

    for mooncard aluval: allow non-timesheetable aac on department

--- project_task_analytic_hr/models/hr_department.py
+++ project_task_analytic_hr/models/hr_department.py
@@ -8,10 +8,6 @@ class HrDepartment(models.Model):
     analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account',
         string='Analytic Account',
-        domain="""[
-            ('timesheetable', '=', True),
-            '|', ('company_id', '=', False), ('company_id', '=', company_id),
-        ]""",
         help='Analytic Account of the Tasks created by'
              ' the employees of the department.'
     )
